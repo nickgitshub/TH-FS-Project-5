1bff3e47bf9e79da42a4545bdc3c4e8a2a301b23
Nick <miller.nick.c@gmail.com>
2020-04-11 22:27:03 -0700

    Update prior to review

---
+++ public_api_request-v1/README.md
@@ -0,0 +1,6 @@
+
+CSS Changes:
+
+changed the background color of the body to a steel blue(#4682B4)
+
+changed the style and color of the card border to solid and navy
--- public_api_request-v1/js/scripts.js
+++ public_api_request-v1/js/scripts.js
@@ -1,15 +1,51 @@
- let userArray = []; 
 
-fetch('https://randomuser.me/api/?results=12')
+/** API FETCHING FUNCTION **/
+
+//used by fetch function to verify that the 200 response was received when calling the API
+function checkStatus (response) {
+	if(response.ok) {
+		return Promise.resolve(response);
+	}else{
+		return Promise.reject(new Error(response.statusText));
+	}
+}
+
+//featches 12 results from the United States, Great Britain, Australia, and New Zealand
+//checks that there's a response from the api
+//then it transforms the response string into JSON
+//then it uses the results of the JSON object to create an array of user objects with the needed data for the page
+//then it creates the cards, creates event listeners on the cards, and adds a search box
+
+let userArray = [];
+fetch('https://randomuser.me/api/?results=12&nat=us,gb,au,nz')
+	.then(checkStatus)
 	.then(response => response.json())
 	.then(data => {
-		console.log(data.results)
-		userArray = createUserArray(data.results);
-		createUserCards(userArray)
+		userArray = createUserArray(data.results)
+		createUserCards(userArray);
 		cardListeners();
 		searchDirectory();
-
 	})
+	.catch(error => console.log('Fetch error', error))
+
+
+
+/** HELPER FUNCTIONS **/
+
+
+//used by createUserArray to transform date of birth from the API to a readable date of birth
+
+function createDOB(date){
+	const DOBObject = new Date(date)
+	const month = DOBObject.getMonth()+1
+	const day = DOBObject.getDate()
+	const year = DOBObject.getFullYear().toString().slice(2)
+
+	return month + "/" + day + "/" + year; 
+};
+
+//uses the fetch response json to create an array of objects that can be easily read and used 
+//for the creation of the user cards
 
 function createUserArray(resultArray){
 	let  = newUserArray = [];
@@ -27,14 +63,8 @@ function createUserArray(resultArray){
 	return newUserArray; 
 }
 
-function createDOB(date){
-	const DOBObject = new Date(date)
-	const month = DOBObject.getMonth()+1
-	const day = DOBObject.getDate()
-	const year = DOBObject.getFullYear().toString().slice(2)
 
-	return month + "/" + day + "/" + year; 
-};
+//iterates throught the userArray, making a user card out of each object, and appending it to the gallery container
 
 function createUserCards(userArray){
 	const gallery = document.getElementById('gallery')
@@ -59,6 +89,9 @@ function createUserCards(userArray){
 
 }
 
+//adds 'click' event listeners to the cards that were created by createUserCards
+//if the event is called, it brings up a modal card with more details
+
 function cardListeners(){
 	cardDivs = document.getElementsByClassName('card');
 	for (let cd of cardDivs){
@@ -73,6 +106,8 @@ function cardListeners(){
 		})
 	}
 }
+
+//when a card is clicked, it brings up a card with addtional details about the clicked user
 
 function addModalCard(user){
 
@@ -103,6 +138,11 @@ function addModalCard(user){
 
    alterModalCard(user);
 }
+
+//creates event listeners for modal card 
+//if the 'X' button is clicked, it closes
+//if 'PREV' or 'NEXT', are created, it takes the index of the current user and then jumps to
+// the previous or the next users
 
 function alterModalCard(user){
 	const modalContainer = document.getElementsByClassName('modal-container')[0]
@@ -137,6 +177,10 @@ function alterModalCard(user){
 
 }
 
+//creates a search form field and appends it to 'search container' div
+//iterates through card names to see if there is match with the current input text
+//hides all of the cards where there isn't a match
+
 function searchDirectory(input){
 	let searchForm = document.createElement("FORM");
 	searchForm.method = 'get';
@@ -153,8 +197,8 @@ function searchDirectory(input){
 	submitButton.addEventListener('click', (e)=>{
 		e.preventDefault();
 
+		const searchInput = document.getElementById('search-input').value;
 		for (let cn of cardNames){
-			const searchInput = document.getElementById('search-input').value;
 			const match = cn.innerText.search(searchInput);
 			if(match > -1){
 				cn.parentNode.parentNode.style.display = '';
